c58afc224e8a334fc800e2adb9610aedf3c149bf
izZERO <crusherh7x@hotmail.com>
2025-11-11 10:03:47 +0300

    signup & signin implemented

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.landing, name="landing")
+    # Signup
+    path('accounts/signup/', views.signup, name='signup'),
+    path("", views.landing, name="landing"),
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def landing(request):
     return render(request,"landing.html")
+
+def signup(request):
+    error_message = ""
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect("landing")
+        else:
+            error_message = "invalid signup - try again later..."
+    form = UserCreationForm()
+    context = {"form": form, "error_message": error_message}
+    return render(request, "registration/signup.html", context)
+
+def profile(request):
+    return render(request, 'users/profile.html')
