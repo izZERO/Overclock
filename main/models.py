a4d97d9d89f9b55ca4690b342c2570454bea5161
izZERO <crusherh7x@hotmail.com>
2025-11-11 09:34:19 +0300

    added Product Model and setup admin page

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name= models.CharField(max_length=50)
+    description= models.CharField(max_length=250)
+    image= models.ImageField(upload_to="main/static/uploads", default="")
+    price= models.FloatField()
+    weight= models.IntegerField()
+    stock= models.IntegerField()
+
